0de85409f609973492fb4d76fe832a44bd15f54f
ALEXANDRE BARBOSA SANTANA <02481639570@PMSE.GOV>
2022-12-21 10:13:45 -0300

    projeto inicial

---
+++ src/main/java/com/api/microservicecc/MicroserviceccApplication.java
@@ -0,0 +1,13 @@
+package com.api.microservicecc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/microservicecc/MicroserviceccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.microservicecc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
